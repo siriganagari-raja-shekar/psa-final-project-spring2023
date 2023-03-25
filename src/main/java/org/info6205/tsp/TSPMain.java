e523dbfabb5fdf15d324b8835d1fa71a0a73da50
siriganagari-raja-shekar <114027488+siriganagari-raja-shekar@users.noreply.github.com>
2023-03-25 12:13:40 -0400

    Setup project

---
+++ src/main/java/org/info6205/tsp/TSPMain.java
@@ -0,0 +1,13 @@
+package org.info6205.tsp;
+
+/**
+ * Hello world!
+ *
+ */
+public class TSPMain
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
