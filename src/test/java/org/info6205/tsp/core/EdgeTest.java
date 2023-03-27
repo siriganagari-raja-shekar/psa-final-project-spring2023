af7840fefa741d1d6b1c10cd480e0cb031ccdfdc
VenkatPavanMunaganti <109862602+VenkatPavanMunaganti@users.noreply.github.com>
2023-03-27 14:40:39 -0400

    Add method to calculate distance in meters
    Added a method to calculate the distance between latitude and longitude

--- src/test/java/org/info6205/tsp/core/EdgeTest.java
+++ src/test/java/org/info6205/tsp/core/EdgeTest.java
@@ -10,10 +10,10 @@ public class EdgeTest {
      * Method to test the implementation of Euclidean distance between vertices
      */
     @Test
-    public void testEuclideanDistance(){
-        Vertex v1= new Vertex(1, 10.0, 20.0);
-        Vertex v2= new Vertex(2, 11.0, 21.0);
+    public void testDistance(){
+        Vertex v1= new Vertex(1, 42.360091919201636, -71.09408974968969);
+        Vertex v2= new Vertex(2, 42.33988862950724, -71.09417558037691);
         Edge edge= new Edge(v1, v2);
-        assertEquals(edge.getWeight(), 1.4142135623730951, 0.1);
+        assertEquals(edge.getWeight(), 2246.517481334295, 0.1);
     }
 }
