7fb16a1b34123a44fc8a227b2a1c2f5e261e63cf
VenkatPavanMunaganti <109862602+VenkatPavanMunaganti@users.noreply.github.com>
2023-04-15 21:24:27 -0400

    Added Util method to convert Eulers/TSP circuit to Graph

--- src/main/java/org/info6205/tsp/util/GraphUtil.java
+++ src/main/java/org/info6205/tsp/util/GraphUtil.java
@@ -1,12 +1,12 @@
 package org.info6205.tsp.util;
 
 import org.apache.lucene.util.SloppyMath;
+import org.info6205.tsp.core.Edge;
+import org.info6205.tsp.core.Graph;
+import org.info6205.tsp.core.UndirectedGraph;
 import org.info6205.tsp.core.Vertex;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class GraphUtil {
 
@@ -31,5 +31,48 @@ public class GraphUtil {
 
         result.add(result.get(0));
         return result;
+    }
+
+    public static List<Edge> removeDuplicateUndirectedEdgesFromMultigraph(Graph graph){
+
+        List<Edge> edgeList = new ArrayList<>(graph.getAllEdges());
+        edgeList.sort((a,b)-> {
+            if(a.getSource() == b.getSource()){
+                return Long.compare(a.getDestination().getId(), b.getDestination().getId());
+            }
+            else
+                return Long.compare(a.getSource().getId(), b.getSource().getId());
+        });
+        Set<String> keysOfEdgesToRemove = new HashSet<>();
+        Set<Edge> edgesToRemove = new HashSet<>();
+        for(Edge edge: edgeList){
+            if(keysOfEdgesToRemove.contains(edge.getSource().getId()+"s"+edge.getDestination().getId()+"d")){
+                edgesToRemove.add(edge);
+            }
+            else{
+                keysOfEdgesToRemove.add(edge.getDestination().getId()+"s"+edge.getSource().getId()+"d");
+            }
+        }
+        edgeList.removeAll(edgesToRemove);
+        return edgeList;
+    }
+
+    public static Graph generateGraphFromEulerianCircuit(List<Vertex> vertices) throws Exception {
+        Graph graph= new UndirectedGraph();
+        for(Vertex v: vertices){
+            boolean isAlreadAdded= false;
+            for(Vertex gv: graph.getAllVertices())
+                if(v.getId() == gv.getId())
+                    isAlreadAdded = true;
+
+            if(!isAlreadAdded)
+                graph.addVertex(v);
+        }
+        for(int i=0; i< vertices.size(); i++){
+            if(i+1 <= vertices.size()-1){
+                graph.addEdge(vertices.get(i), vertices.get(i+1));
+            }
+        }
+        return graph;
     }
 }
